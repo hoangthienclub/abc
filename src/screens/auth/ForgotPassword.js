263d6c51db0290c9a61956310acfc2540c4c0e38
Thien Tran <thientran@thientrans-MacBook-Pro-5.local>
2019-06-04 11:40:22 +0700

    forgot password

--- src/screens/auth/ForgotPassword.js
+++ src/screens/auth/ForgotPassword.js
@@ -1,16 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
-import Button from '../../components/Button';
-import Input from '../../components/Input';
-import Title from '../../components/Title';
-class ForgotPassword extends Component {
+import { View, Text, Image, TouchableOpacity, ImageBackground, TextInput, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-    constructor(props) {
-        super(props);
-        this.state = { 
-            text: ''
-        };
-    }
+class ForgotPassword extends Component {
 
     static navigationOptions = {
         headerStyle: {
@@ -20,21 +12,74 @@ class ForgotPassword extends Component {
 
     render() {
         return (
-            <View style={{ flex: 1, paddingHorizontal: 35, marginTop: 30 }}>
-                <View style={{ flex: 1 }}>
-                    <Title title='Forgot Password'/>
-                    <Text style={{ fontSize: 14, color: '#ABB4BD', marginTop: 20 }}>Please enter your registered email or mobile to reset your password</Text>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginTop: 50 }}>
-                        <Input placeholder='Email / Mobile number'/>
+            <ImageBackground source={require('../../assets/Background.png')} style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} resizeMode='cover'>
+                <View style={{backgroundColor: '#fff', width: 700, height: 350, borderRadius: 5 }}>
+                    <View style={ styles.header }>
+                        <TouchableOpacity style={{ position: "absolute", top: 0, left: 0, height: 80, width: 80, justifyContent: 'center', alignItems: 'center' }}>
+                            <Icon
+                                name='ios-arrow-round-back'
+                                size={50}
+                                color='#fff'
+                            />
+                        </TouchableOpacity>
+                        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                            <Text style={{color: '#fff', fontSize: 28 }}>Forgot Password</Text>
+                        </View>
                     </View>
-                    <View style={{ flex: 2, justifyContent: 'center' }}>
-                        <Button title='Recover Password'/>
+                    <View style={{ flex: 1, flexDirection: 'column', paddingHorizontal: 50}}>
+                        <View style={{ flex: 1 }}>
+                            <Text>Username</Text>
+                            <TextInput
+                                onChangeText={value => onChangeText(type, value)}
+                                style={ styles.input }
+                                secureTextEntry={ false }
+                                autoCapitalize='none'
+                                autoCorrect={false}
+                            />
+                        </View>
+                    </View>
+                    <View style={{ flex: 1.5 }}>
+                        <View style={{ flex: 1, justifyContent: 'center', flexDirection: 'row' }}>
+                            <TouchableOpacity style={{ flex: 1 }}>
+                                <View style={ styles.buttonLogin }>
+                                    <Text style={{ fontSize: 20, color: "#fff" }}>Login</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 </View>
-                <View style={{ flex: 1 }}></View>
-            </View>
+            </ImageBackground>
         )
     }
 }
 
 export default ForgotPassword;
+
+
+const styles =  StyleSheet.create({
+    header: {
+        backgroundColor: '#6291FF',
+        // flex: 1,
+        width: '100%',
+        height: 80,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    input: {
+        borderColor: '#CFD5DA', 
+        borderWidth: 1, 
+        paddingVertical: 10, 
+        width: '100%' ,
+        borderRadius: 5,
+        height: 50,
+        marginTop: 15
+    },
+    buttonLogin: {
+        backgroundColor: '#6291FF',
+        height: 50, 
+        width: '100%', 
+        justifyContent: 'center', 
+        alignItems: 'center',
+        borderRadius: 5
+    }
+})
--- src/screens/auth/Login.js
+++ src/screens/auth/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Image, TouchableOpacity, ImageBackground } from 'react-native';
+import { View, Text, Image, TouchableOpacity, ImageBackground, TextInput, StyleSheet } from 'react-native';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import { connect } from 'react-redux';
@@ -36,86 +36,54 @@ class Login extends Component {
         const { email, password } = this.state
         this.props.login(email, password)
     }
+    
 
     render() {
         const { email, password } = this.state;
         return (
-            <ImageBackground source={require('../../assets/Background.png')} style={{ flex: 1 }} resizeMode='cover'>
-                <View style={{ flex: 1, paddingHorizontal: 35, backgroundImage: ''}}>
-                    <View style={{ flex: 2.5 }}>
+            <ImageBackground source={require('../../assets/Background.png')} style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} resizeMode='cover'>
+                <View style={{ backgroundColor: '#fff', paddingHorizontal: 50, width: 800, height: 550 }}>
+                    <View style={{ flex: 2 }}>
                         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                             <Image
                                 source={require('../../assets/logo.png')}
-                                style={{ width: 81.76, height: 70.56 }}
                             />
-                            <Text style={{ fontSize: 21.5, color: '#1D2029' }}>Vermo</Text>
                         </View>
                     </View>
-                    <View style={{ flex: 1, flexDirection: 'row' }}>
-                        <View style={{ flex: 1, justifyContent: 'center', flexDirection: 'row' }}>
-                            <TouchableOpacity style={{ flex: 1 }}>
-                                <View style={{
-                                    backgroundColor: '#FFFFFF', width: '100%', height: 40, borderWidth: 1, borderColor: 'rgba(171,180,189,0.35)', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginRight: 20, borderRadius: 3, shadowOpacity: 0.5,
-                                    shadowColor: 'rgba(171,180,189,0.21)',
-                                    shadowOffset: { height: 10, width: 0 }
-                                }}>
-                                    <Image
-                                        source={require('../../assets/facebook.png')}
-                                        style={{ width: 16, height: 16 }}
-                                    />
-                                    <Text style={{ fontSize: 12, color: '#1D2029', marginLeft: 10 }}>Facebook</Text>
-                                </View>
-                            </TouchableOpacity>
-                        </View>
-                        <View style={{ flex: 1, justifyContent: 'center', flexDirection: 'row' }}>
-                            <TouchableOpacity style={{ flex: 1 }}>
-                                <View style={{
-                                    backgroundColor: '#FFFFFF', width: '100%', height: 40, borderWidth: 1, borderColor: 'rgba(171,180,189,0.35)', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginLeft: 20, borderRadius: 3, shadowOpacity: 0.5,
-                                    shadowColor: 'rgba(171,180,189,0.21)',
-                                    shadowOffset: { height: 10, width: 0 }
-                                }}>
-                                    <Image
-                                        source={require('../../assets/google.png')}
-                                        style={{ width: 16, height: 16 }}
-                                    />
-                                    <Text style={{ fontSize: 12, color: '#1D2029', marginLeft: 10 }}>Google</Text>
-                                </View>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                    <View style={{ flex: 2 }}>
+                    <View style={{ flex: 2, flexDirection: 'column'}}>
                         <View style={{ flex: 1 }}>
-                            <Input placeholder='Email' 
-                                value={ email }
-                                onChangeText={ this.onChangeText }
-                                type='email'
+                            <Text>Username</Text>
+                            <TextInput
+                                onChangeText={value => onChangeText(type, value)}
+                                style={ styles.input }
+                                secureTextEntry={ false }
+                                autoCapitalize='none'
+                                autoCorrect={false}
                             />
                         </View>
                         <View style={{ flex: 1 }}>
-                            <Input placeholder='Password' 
-                                value={ password }
-                                onChangeText={ this.onChangeText }
-                                secureTextEntry={ true }
-                                type='password'
+                            <Text>Password</Text>
+                            <TextInput
+                                onChangeText={value => onChangeText(type, value)}
+                                style={ styles.input }
+                                secureTextEntry={ false }
+                                autoCapitalize='none'
+                                autoCorrect={false}
                             />
-                        </View>
-                        <View style={{ alignItems: 'flex-end', paddingTop: 20 }}>
-                            <Text style={{ fontSize: 12, color: '#FF1654' }}
-                                onPress={() => this.props.navigation.navigate('ForgotPassword')}
-                            >Forgot Password?</Text>
                         </View>
                     </View>
-                    <View style={{ flex: 2 }}>
-                        <View style={{ flex: 2, justifyContent: 'center' }}>
-                            <Button title='Login' 
-                                onPress={ () => this.login() }
-                            />
+                    <View style={{ flex: 1.5 }}>
+                        <View style={{ flex: 1, justifyContent: 'center', flexDirection: 'row' }}>
+                            <TouchableOpacity style={{ flex: 1 }}>
+                                <View style={ styles.buttonLogin }>
+                                    <Text style={{ fontSize: 20, color: "#fff" }}>Login</Text>
+                                </View>
+                            </TouchableOpacity>
                         </View>
                         <View style={{ flexDirection: 'row', flex: 1, justifyContent: 'center' }}>
-                            <Text style={{ fontSize: 12, color: '#ABB4BD' }}>Don't have an account?</Text>
-                            <Text style={{ fontSize: 12, color: '#FF1654' }}
-                                onPress={() => this.props.navigation.navigate('Register')}
-                            > Register Now</Text>
+                            <Text style={{ fontSize: 15, color: '#4A90E2', textDecorationLine: 'underline' }}
+                                onPress={() => this.props.navigation.navigate('ForgotPassword')}
+                            > Forgot password</Text>
                         </View>
                     </View>
                 </View>
@@ -147,3 +115,24 @@ const mapStateToProps = state => ({
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
+
+
+const styles =  StyleSheet.create({
+    input: {
+        borderColor: '#CFD5DA', 
+        borderWidth: 1, 
+        paddingVertical: 10, 
+        width: '100%' ,
+        borderRadius: 5,
+        height: 50,
+        marginTop: 15
+    },
+    buttonLogin: {
+        backgroundColor: '#6291FF',
+        height: 50, 
+        width: '100%', 
+        justifyContent: 'center', 
+        alignItems: 'center',
+        borderRadius: 5
+    }
+})
